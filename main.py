cba28610561b5ba8cd4632fa37e831289f5d8bbb
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-05-28 08:07:31 -0700

    create enemy and its movement

--- main.py
+++ main.py
@@ -55,4 +55,37 @@ turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
 
 
+# Enemy
+enemy = turtle.Turtle()
+enemy.color("red")
+enemy.shape("circle")
+enemy.penup()
+enemy.speed(0)
+enemy.setposition(-200, 250)
+
+enemyspeed = 2
+
+
+
+# Game loop
+while True:
+    # Move enemy
+    x = enemy.xcor()
+    x += enemyspeed
+    enemy.setx(x)
+
+    # Move enemy down and bounce on the right side
+    if enemy.xcor > 280:
+        y = enemy.ycor()
+        y -= 40
+        enemyspeed *= -1
+        enemy.sety(y)
+
+    # Move enemy down and bounce on left side
+    if enemy.xcor() < -280:
+        y = enemy.ycor()
+        y -= 40
+        enemyspeed *= -1
+        enemy.sety(y)
+
 delay = input("Press enter to finish")

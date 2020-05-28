d1cb59909f3011e6fbe1e33eafd6a39bd8316131
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-05-28 09:08:18 -0700

    add multiple enemies

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import turtle
 import os
+import math
+import random
 
 # Screen
 wn = turtle.Screen()
@@ -49,19 +51,72 @@ def move_right():
         x = 280
     player.setx(x)
 
-### Keyboard bindings
+## Bullet
+### Ready to fire
+bulletstate = "ready"
+
+### Bullet firing
+def fire_bullet():
+    # Declare bulletstate as global if it needs change
+    global bulletstate
+    if bulletstate == "ready":
+        bulletstate = "fire"
+        # Move bullet
+        x = player.xcor()
+        y = player.ycor() + 10
+        bullet.setposition(x, y)
+        bullet.showturtle()
+
+def isCollision(t1, t2):
+    distance = math.sqrt(math.pow(t1.xcor() - t2.xcor(), 2) + math.pow(t1.ycor() - t2.ycor(), 2))
+    if distance < 15:
+        return True
+    else:
+        return False
+
+
+# Keyboard bindings
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
+turtle.onkey(fire_bullet, "space")
+
+
+# Player bullet
+bullet = turtle.Turtle()
+bullet.color("yellow")
+bullet.shape("triangle")
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5, 0.5)
+bullet.hideturtle()
+
+bulletspeed = 20
+
+
 
 
 # Enemy
-enemy = turtle.Turtle()
-enemy.color("red")
-enemy.shape("circle")
-enemy.penup()
-enemy.speed(0)
-enemy.setposition(-200, 250)
+## Number of enemies
+number_of_enemies = 5
+
+## List of enemies
+enemies = []
+
+## Add enemies to list
+for i in range(number_of_enemies):
+    # Create enemy
+    enemies.append(turtle.Turtle())
+
+for enemy in enemies:
+    enemy.color("red")
+    enemy.shape("circle")
+    enemy.penup()
+    enemy.speed(0)
+    x = random.randint(-200, 200)
+    y = random.randint(100, 250)
+    enemy.setposition(x, y)
 
 enemyspeed = 2
 
@@ -69,23 +124,70 @@ enemyspeed = 2
 
 # Game loop
 while True:
-    # Move enemy
-    x = enemy.xcor()
-    x += enemyspeed
-    enemy.setx(x)
 
-    # Move enemy down and bounce on the right side
-    if enemy.xcor > 280:
-        y = enemy.ycor()
-        y -= 40
-        enemyspeed *= -1
-        enemy.sety(y)
+    for enemy in enemies:
+        # Move enemy
+        x = enemy.xcor()
+        x += enemyspeed
+        enemy.setx(x)
 
-    # Move enemy down and bounce on left side
-    if enemy.xcor() < -280:
-        y = enemy.ycor()
-        y -= 40
-        enemyspeed *= -1
-        enemy.sety(y)
+        # Move enemy down and bounce on the right side
+        if enemy.xcor() > 280:
+            y = enemy.ycor()
+            y -= 40
+            enemyspeed *= -1
+            enemy.sety(y)
+
+        # Move enemy down and bounce on left side
+        if enemy.xcor() < -280:
+            y = enemy.ycor()
+            y -= 40
+            enemyspeed *= -1
+            enemy.sety(y)
+
+        # Check for bullet/enemy collision
+        if isCollision(bullet, enemy):
+            # reset bullet
+            bullet.hideturtle()
+            bulletstate = "ready"
+            bullet.setposition(0, -400)
+
+            # reset enemy
+            x = random.randint(-200, 200)
+            y = random.randint(100, 250)
+            enemy.setposition(x, y)
+
+        if isCollision(player, enemy):
+            player.hideturtle()
+            enemy.hideturtle()
+            print("GAME OVER")
+            break
+
+    # Move bullet
+    if bulletstate == "fire":
+        y = bullet.ycor()
+        y += bulletspeed
+        bullet.sety(y)
+
+    # Check if bullet hit top border
+    if bullet.ycor() > 275:
+        bullet.hideturtle()
+        bulletstate = "ready"
+
+    # Check for bullet/enemy collision
+    if isCollision(bullet, enemy):
+        # reset bullet
+        bullet.hideturtle()
+        bulletstate = "ready"
+        bullet.setposition(0, -400)
+
+        # reset enemy
+        enemy.setposition(-200, 250)
+
+    if isCollision(player, enemy):
+        player.hideturtle()
+        enemy.hideturtle()
+        print("GAME OVER")
+        break
 
 delay = input("Press enter to finish")

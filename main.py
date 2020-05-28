4dbbebd95f1253f093aafbaab671b3ac0f4c73c1
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-05-28 07:40:06 -0700

    create border

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ wn.bgcolor("black")
 wn.title("Space Invaders")
 
 
+## Border
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300, -300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
 
 
 
